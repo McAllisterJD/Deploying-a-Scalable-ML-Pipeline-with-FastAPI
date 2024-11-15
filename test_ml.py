1b1123128954236551f2185268d49dd5c229e2b6
McAllisterJD <jacobdmcallister@gmail.com>
2024-11-14 18:49:48 -0700

    Update test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 def train_dataset():
-    data_path = os.path.join(os.path.dirname(__file__), 'data', 'census.csv')
+    data_path = '/home/runner/work/Deploying-a-Scalable-ML-Pipeline-with-FastAPI/Deploying-a-Scalable-ML-Pipeline-with-FastAPI/data/census.csv'
     df = pd.read_csv(data_path)
     train, test = train_test_split(df, test_size=0.2, random_state=42)
     X_train, y_train, encoder, lb = process_data(
